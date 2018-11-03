d744ca60261401401132732241e1c4a1bd578a9a
Dacio Romero <dacioromero@gmail.com>
2018-11-03 16:57:26 -0700

    Add MVP

--- app.js
+++ app.js
@@ -1,13 +1,60 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-let id = 0;
+app.use(bodyParser.json());
 
-const animals = {}
+let id = 2;
 
-const port = 3000
+// TODO: Use more advanced data type
+const animals = {0: 'cat', 1: 'dog'}
 
-app.listen(port, () => {
-  console.log(`Server Starts on ${port}`);
+// INDEX Animals
+app.get('/animals', (req, res) => {
+    res.status(200).send(animals);
 });
+
+// SHOW Animal
+app.get('/animals/:id', (req, res) => {
+    const animalId = req.params.id;
+
+    res.status(200).send({ [animalId]: animals[req.params.id] });
+});
+
+// CREATE Animal
+app.post('/animals', (req, res) => {
+    const animalId = id++;
+    animals[animalId] = req.body.name;
+
+    res.status(200).send({ [animalId]: animals[animalId] });
+});
+
+// UPDATE Animal
+app.put('/animals/:id', (req, res) => {
+    const animalId = req.params.id;
+    animals[animalId] = req.body.name;
+
+    res.status(200).send({ [animalId]: animals[animalId] });
+});
+
+// DESTROY Animal D:
+app.delete('/animals/:id', (req, res) => {
+    const animalId = req.params.id;
+    const animal = animals[animalId];
+
+    delete animals[animalId];
+
+    res.status(200).send({ [animalId]: animal });
+});
+
+// LISTENER
+if (require.main === module) {
+    const port = process.env.PORT || 3000;
+
+    app.listen(port, () => {
+        console.log(`App listening on port ${port}!`);
+    });
+}
+
+module.exports = app;
